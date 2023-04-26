acfb20c82f6552c5074eba440d9caadf57790ad5
Pavel Surkov <surkovv.pavel@gmail.com>
2023-04-27 01:25:44 +0700

    adds full voice message hanlde

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ const bot = new Telegraf(config.get('TELEGRAM_TOKEN'));
 
 bot.on(message('voice'), async (ctx) => {
   try {
-    await ctx.reply(code('Waiting for server response...'));
+    await ctx.reply(code('Waiting for voice message processing...'));
 
     const userId = String(ctx.message.from.id);
     const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id);
@@ -17,11 +17,14 @@ bot.on(message('voice'), async (ctx) => {
     const mp3Path = await ogg.toMp3(oggPath, userId);
 
     const text = await openai.transcription(mp3Path);
-    // const response = await openai.chat(text);
 
     await ctx.reply(`Your message: ${text}`);
+    await ctx.reply(code('Waiting for server response...'));
 
-    // await ctx.reply(JSON.stringify(text, null, 2));
+    const messages = [{ role: openai.roles.USER, content: text }];
+    const response = await openai.chat(messages);
+
+    await ctx.reply(response.content);
   } catch (err) {
     console.log(`Error while voice message`, err);
   }
--- src/openai.js
+++ src/openai.js
@@ -3,6 +3,12 @@ import { Configuration, OpenAIApi } from 'openai';
 import { createReadStream } from 'fs';
 
 class OpenAi {
+  roles = {
+    ASSISTANT: 'assistant',
+    USER: 'user',
+    SYSTEM: 'system',
+  };
+
   constructor(apiKey) {
     const configuration = new Configuration({
       apiKey: apiKey,
@@ -11,7 +17,18 @@ class OpenAi {
     this.openai = new OpenAIApi(configuration);
   }
 
-  async chat() {}
+  async chat(messages) {
+    try {
+      const response = await this.openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages,
+      });
+
+      return response.data.choices[0].message;
+    } catch (err) {
+      console.log('Error whild gpt chat', err.message);
+    }
+  }
 
   async transcription(filePath) {
     try {
@@ -22,7 +39,7 @@ class OpenAi {
 
       return response.data.text;
     } catch (err) {
-      console.log('Error while transcription', err);
+      console.log('Error while transcription', err.message);
     }
   }
 }
